84f74c46bbe52b2b29e57f54d0b31608a7add989
Vince Borbély <borbely.vince.11@gmail.com>
2026-07-16 12:48:58 +0200

    dm2q: Add display brightness to displayconfig
    Change-Id: I1af1709cf6c1ac1307b3e8a0169614d5f53c0085

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Display config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947118961383299.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.dm2q.rc
